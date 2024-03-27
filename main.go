29c58e9a38c48d95b98f7fba6e9a3b4645677ffd
fernandoDelpo <fernandodelpozzi@gmail.com>
2024-03-27 18:01:57 -0300

    Web Basic III & IV

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type User struct {
@@ -32,7 +33,7 @@ func init() {
 		FirstName: "Francesca",
 		LastName:  "Del Pozzi",
 		Email:     "fran@example.com",
-	},{
+	}, {
 		ID:        "4",
 		FirstName: "Maxima",
 		LastName:  "Perez",
@@ -43,25 +44,28 @@ func init() {
 func main() {
 
 	http.HandleFunc("/users", UserServer)
-
+	fmt.Println("Starting server on port :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
 
 func UserServer(w http.ResponseWriter, r *http.Request) {
-	var status int
+
 	switch r.Method {
 	case http.MethodGet:
 		GetAllUser(w)
 	case http.MethodPost:
-		status = 200
-		w.WriteHeader(status)
-		fmt.Fprintf(w, `{"status": %d, "message": "%s"}`, status, "success method POST")
+		decode := json.NewDecoder(r.Body)
+		var u User
+		err := decode.Decode(&u)
+		if err != nil {
+			MsgResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		PostUser(w, u)
 
 	default:
-		status = 404
-		w.WriteHeader(status)
-		fmt.Fprintf(w, `{"status": %d, "message": "%s"}`, status, "NOT FOUND")
+		InvalidMethod(w)
 	}
 
 }
@@ -70,8 +74,51 @@ func GetAllUser(w http.ResponseWriter) {
 	DataResponse(w, http.StatusOK, users)
 }
 
+func PostUser(w http.ResponseWriter, data interface{}) {
+
+	user := data.(User)
+
+	if user.FirstName  == "" {
+		MsgResponse(w, http.StatusBadRequest, "First name is required.")
+		return
+	} 
+	
+	if user.LastName  == "" {
+		MsgResponse(w, http.StatusBadRequest, "Last name is required.")
+		return
+	}
+	if user.Email  == "" {
+		MsgResponse(w, http.StatusBadRequest, "Email is required.")
+		return
+	}
+
+	id := len(users) + 1
+	idString := strconv.Itoa(id)
+	user.ID = idString
+	users = append(users, user)
+	DataResponse(w, http.StatusCreated, user)
+
+}
+
+func MsgResponse(w http.ResponseWriter, status int, message string) {
+
+	w.WriteHeader(status)
+	fmt.Fprintf(w, `{"status": %d, "message": "%s"}`, status, message)
+}
+
 func DataResponse(w http.ResponseWriter, status int, users interface{}) {
-	value, _ := json.Marshal(users)
+	value, err := json.Marshal(users)
+	if err != nil {
+		MsgResponse(w, http.StatusInternalServerError, err.Error())
+		return
+	}
 	w.WriteHeader(status)
 	fmt.Fprintf(w, `{"status": %d, "data": %s}`, status, value)
+}
+
+func InvalidMethod(w http.ResponseWriter) {
+
+	status := http.StatusNotFound
+	w.WriteHeader(status)
+	fmt.Fprintf(w, `{"status": %d, "message": "method doesn't exits"}`, status)
 }
